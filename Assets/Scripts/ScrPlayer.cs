a715c09b2f8c24a11ac0095c799ae5ade2158955
Paula Moreta <moretaap@campusemav.com>
2021-01-18 11:02:04 +0100

    Classe 18/01/2021

---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Script utilitzat per controlar el player
+/// AUTOR:  Paula Moreta
+/// DATA:   18/01/2021
+/// VERSIÓ: 1.0
+/// CONTROL DE VERSIONS
+///         1.0: primera versió. Moviment de la nau amb tecles i físiques
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField] float velocitat = 10f;
+    Vector2 movi = new Vector2();   // per calcular moviment
+    Rigidbody2D rb;                 // per accedir al component rigidbody
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //donem valor al rb
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
