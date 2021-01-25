5830f6cc5b09d4b8aa348b394e437bf6ca3009cd
Paula Moreta <moretaap@campusemav.com>
2021-01-25 09:34:53 +0100

    Classe 25/01/2021 versió 2

--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -9,8 +9,10 @@ using UnityEngine;
 /// AUTOR:  Paula Moreta
 /// DATA:   18/01/2021
 /// VERSIÓ: 1.0
+///         2.0
 /// CONTROL DE VERSIONS
 ///         1.0: primera versió. Moviment de la nau amb tecles i físiques
+///         2.0: segona versió.  Implementació de joystick
 /// ----------------------------------------------------------------------------------
 /// </summary>
 
@@ -20,6 +22,14 @@ public class ScrPlayer : MonoBehaviour
     Vector2 movi = new Vector2();   // per calcular moviment
     Rigidbody2D rb;                 // per accedir al component rigidbody
 
+    //************************************Gestió shot**********************************
+    [SerializeField] GameObject missil; //element a instanciar, arrosseguem prefab
+    [SerializeField] Transform cano; //d'on surt el tret
+
+    //*************************************cool down********************************
+    [SerializeField] float cadencia = 0.5f; //dispararà cada 5 dècimes de segon
+    float crono = 0f; //per comptar el temps de cadència
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //donem valor al rb
@@ -28,11 +38,23 @@ public class ScrPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movi.x = Input.GetAxis("Horizontal");
-        movi.y = Input.GetAxis("Vertical");
+        movi.x = ETCInput.GetAxis("Horizontal") * velocitat;
+        movi.y = ETCInput.GetAxis("Vertical") * velocitat;
+
+        if (ETCInput.GetButton("Shoot") && crono > cadencia) Dispara();
+        crono += Time.deltaTime;
+
+        if (ETCInput.GetButtonUp("Shoot")) crono = cadencia; //permet disparar ràpid amb múltiples clices
     }
     private void FixedUpdate()
     {
-        rb.velocity = movi;
+        rb.velocity = movi; //Apliquem velocitat amb fisiques
+    }
+    void Dispara()
+    {
+        Instantiate(missil, cano.position, cano.rotation);
+        Instantiate(missil, cano.position, Quaternion.Euler (new Vector3(0, 0, 33)));
+        Instantiate(missil, cano.position, Quaternion.Euler (new Vector3(0, 0, -33)));
+        crono = 0;
     }
 }
---
+++ Assets/Scripts/ScrShot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrShot : MonoBehaviour
+{
+    [SerializeField] float vel = 20f;
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(vel * Time.deltaTime, 0, 0);
+    }
+}
